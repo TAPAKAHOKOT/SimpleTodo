f828ac9e008bbcd10ef4edaedc1d9b13a803371a
TAPAKAHOKOT <tapakahokot@gmail.com>
2025-06-24 13:50:25 +0200

    Добавил запуск в докере и postgresql

--- back/tasks/api/serializers.py
+++ back/tasks/api/serializers.py
@@ -2,12 +2,22 @@ from rest_framework import serializers
 from tasks import choices, models
 
 class TaskListSerializer(serializers.Serializer):
+    pk = serializers.IntegerField()
     name = serializers.CharField(max_length=32)
 
     def create(self, validated_data):
         return models.TaskList.objects.create(**validated_data)
 
+    def update(self, instance, validated_data):
+        if not validated_data or 'name' not in validated_data:
+            return instance
+        
+        instance.name = validated_data.get('name')
+        instance.save()
+        return instance
+
 class TaskSerializer(serializers.Serializer):
+    pk = serializers.IntegerField()
     title = serializers.CharField(max_length=32)
     
     is_completed = serializers.BooleanField(required=False)
@@ -20,3 +30,11 @@ class TaskSerializer(serializers.Serializer):
 
     def create(self, validated_data):
         return models.Task.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        for field in validated_data:
+            if hasattr(instance, field):
+                setattr(instance, field, validated_data[field])
+        
+        instance.save()
+        return instance
